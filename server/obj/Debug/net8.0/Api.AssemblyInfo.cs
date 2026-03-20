68c102290d673bd80b439128a1ff777c87a5f909
Juris <jurisbandenieks90@gmail.com>
2026-03-20 13:26:38 -0700

    feat: Update navigation styles and enhance Docker configuration for client and API services

--- server/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ba8f7376b8b58eb1bf00200b0072d981e068ff0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af4d97dcdd9d111ff65120da2d5c42024ce72341")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
